1336c3e88fa8e45d9bc70bf1d6dcbb61387e415d
Maya Soloveva <maya.soloveva2015@ya.ru>
2019-04-13 13:48:58 +0300

    Поменяла шаблонизатор на gulp-pug, pug-php-filter, расширение на .pug

--- tasks/templates.js
+++ tasks/templates.js
@@ -1,7 +1,8 @@
 import gulp from 'gulp';
 import gulpIf from 'gulp-if';
 import plumber from 'gulp-plumber';
-import jade from 'gulp-jade-php';
+import pug from 'gulp-pug';
+import pugPHPFilter from 'pug-php-filter';
 import pugLinter from 'gulp-pug-linter';
 import prettify from 'gulp-jsbeautifier';
 import inheritance from 'gulp-jade-inheritance';
@@ -20,12 +21,12 @@ const data = {
 };
 
 gulp.task('templates', () => (
-	gulp.src('app/**/*.jade')
+	gulp.src('app/**/*.pug')
 		.pipe(plumber({errorHandler: errorHandler(`Error in \'templates\' task`)}))
-		.pipe(cached('jade'))
+		.pipe(cached('pug'))
 		.pipe(gulpIf(global.watch, inheritance({basedir: 'app'})))
 		.pipe(filter(file => /app[\\\/]pages/.test(file.path)))
-		.pipe(jade({basedir: 'app', data}))
+		.pipe(pug({ basedir: 'app', data, filters: { php: pugPHPFilter } }))
 		.pipe(gulpIf(process.env.PRETTIFY !== 'false', prettify({
 			braceStyle: 'expand',
 			indentWithTabs: true,
@@ -47,7 +48,7 @@ gulp.task('templates', () => (
 
 gulp.task('templates:lint', () =>
 	gulp
-		.src('app/**/*.jade')
+		.src('app/**/*.pug')
 		.pipe(pugLinter())
 		.pipe(pugLinter.reporter('fail'))
 );
